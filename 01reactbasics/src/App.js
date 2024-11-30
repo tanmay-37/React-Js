0f1012b474d10609a20ca2f64f013e7eda0cf534
tanmay-37 <talekartk@rknec.edu>
2024-11-30 19:19:53 +0530

    react basics

---
+++ 01reactbasics/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Chai aur react | TT</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./chai"
+
+function App() {
+  const username = "Chai tayar hai"
+  return (
+    <h3>chai is ready | { username}</h3>
+  )
+
+ 
+}
+
+export default App
